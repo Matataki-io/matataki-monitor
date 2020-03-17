7e01b4390db14e75954a391f6613e24cedb9d379
shellteo <shellteo@163.com>
2020-03-17 11:42:38 +0800

    🎉init project

---
+++ index.js
@@ -0,0 +1,28 @@
+const Koa = require('koa')
+const app = new Koa()
+const bodyParser = require('koa-bodyparser')
+const { logger, accessLogger } = require('./logger');
+const exec = require('child_process').exec
+// https://www.alibabacloud.com/help/zh/doc-detail/60714.htm
+const config = require('./config')
+
+app.use(accessLogger())
+
+app.use(bodyParser())
+
+app.use(async (ctx) => {
+    if ( ctx.url === '/' && ctx.method === 'GET' ) {
+        ctx.body = 'get hello monitor'
+    } else if ( ctx.url === '/' && ctx.method === 'POST' ) {
+        const postData = ctx.request.body
+        logger.error(postData);
+        ctx.body = 'post hello monitor'
+        if (postData.userId === config.userId
+            && postData.alertName === config.alertName
+            && postData.alertState === config.alertState) {
+            exec('bash restart.sh')
+        }
+    }
+})
+app.listen(3033)
+console.log('server running at localhost:3033')
---
+++ restart.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This is our restart backend script.
+# cd /usr/local/smart-signature/test/smart-signature-backend
+# npm stop
+# npm start
